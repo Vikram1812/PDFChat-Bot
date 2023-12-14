5d30702f26d6680cc4bc45765ecd11d44a77fca6
Vikram <80635017+Vikram1812@users.noreply.github.com>
2023-12-14 22:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
+    # or you can use HuggingFace Model instead that is mentioned below
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
